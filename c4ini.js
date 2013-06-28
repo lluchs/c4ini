9a920075acac8c9a194735429afdd64e0a625f27
Luchs <Luchs@euirc.eu>
2013-06-29 00:00:32 +0200

    Expand tabs to spaces

--- c4ini.js
+++ c4ini.js
@@ -9,16 +9,16 @@ const entryRegex = /^\s*(\w+)=(.+)/
 var indentLevel = function(line) {
   var level = 0, i = 0
   while (line.charAt(i++) == ' ')
-	if (i % INDENTSIZE == 0)
-	  level++
+    if (i % INDENTSIZE == 0)
+      level++
   return level
 }
 
 // Calls f for non-empty lines.
 var skipEmpty = function(f) {
   return function(line) {
-	if (!/^\s*$/.test(line))
-	  f.apply(this, arguments)
+    if (!/^\s*$/.test(line))
+      f.apply(this, arguments)
   }
 }
 
@@ -27,59 +27,59 @@ module.exports = function(ini) {
   var stack = [obj]
 
   var parseLine = function(line, lineNumber) {
-	var level = indentLevel(line)
-	var match
-	if (match = sectionRegex.exec(line)) {
-	  // This is a section header, create a new object.
-	  var key = match[1]
-	  // Check indentation.
-	  if (level == stack.length) {
-		// One level deeper: Put the enclosing object on the stack.
-		stack.push(current)
-	  }
-	  else if (level < stack.length - 1) {
-		// Higher up - remove objects from the stack.
-		stack.length = level + 1
-	  }
-	  else if (level > stack.length) {
-		throw new Error('Invalid indentation in line ' + (lineNumber + 1))
-	  }
-	  // Create the new object.
-	  current = {}
-	  // Check whether there is already a section with the same name.
-	  if (stack[level][key]) {
-		// We might have to create an array.
-		if (Array.isArray(stack[level][key])) {
-		  // There is already one.
-		  stack[level][key].push(current)
-		}
-		else {
-		  // Put the object there into a new array.
-		  var tmp = stack[level][key]
-		  stack[level][key] = [tmp, current]
-		}
-	  }
-	  else {
-		// We're first.
-		stack[level][key] = current
-	  }
-	}
-	else if (match = entryRegex.exec(line)) {
-	  // Check indentation.
-	  if (level < stack.length - 1) {
-		// Reduce stack size, moving the current item.
-		stack.length = level + 2
-		current = stack[level + 1]
-	  }
-	  else if (level >= stack.length) {
-		throw new Error('Key/value item has wrong indentation in line ' + (lineNumber + 1))
-	  }
-	  var key = match[1], value = match[2]
-	  current[key] = value
-	}
-	else {
-	  throw new Error('Parse error in line ' + (lineNumber + 1))
-	}
+    var level = indentLevel(line)
+    var match
+    if (match = sectionRegex.exec(line)) {
+      // This is a section header, create a new object.
+      var key = match[1]
+      // Check indentation.
+      if (level == stack.length) {
+        // One level deeper: Put the enclosing object on the stack.
+        stack.push(current)
+      }
+      else if (level < stack.length - 1) {
+        // Higher up - remove objects from the stack.
+        stack.length = level + 1
+      }
+      else if (level > stack.length) {
+        throw new Error('Invalid indentation in line ' + (lineNumber + 1))
+      }
+      // Create the new object.
+      current = {}
+      // Check whether there is already a section with the same name.
+      if (stack[level][key]) {
+        // We might have to create an array.
+        if (Array.isArray(stack[level][key])) {
+          // There is already one.
+          stack[level][key].push(current)
+        }
+        else {
+          // Put the object there into a new array.
+          var tmp = stack[level][key]
+          stack[level][key] = [tmp, current]
+        }
+      }
+      else {
+        // We're first.
+        stack[level][key] = current
+      }
+    }
+    else if (match = entryRegex.exec(line)) {
+      // Check indentation.
+      if (level < stack.length - 1) {
+        // Reduce stack size, moving the current item.
+        stack.length = level + 2
+        current = stack[level + 1]
+      }
+      else if (level >= stack.length) {
+        throw new Error('Key/value item has wrong indentation in line ' + (lineNumber + 1))
+      }
+      var key = match[1], value = match[2]
+      current[key] = value
+    }
+    else {
+      throw new Error('Parse error in line ' + (lineNumber + 1))
+    }
   }
 
   ini.split("\n").forEach(skipEmpty(parseLine))
--- test/c4ini.js
+++ test/c4ini.js
@@ -44,19 +44,19 @@ vows.describe('c4ini').addBatch({
       topic: parse,
 
       'returns an object with the section': function(obj) {
-	assert.include(obj, 'section')
+        assert.include(obj, 'section')
       },
       'the object': {
-	topic: function(obj) { return obj.section },
+        topic: function(obj) { return obj.section },
 
-	'includes key1': function(section) {
-	  assert.include(section, 'key1')
-	  assert.equal(section.key1, 'value1')
-	},
-	'includes key2': function(section) {
-	  assert.include(section, 'key2')
-	  assert.equal(section.key2, 'value2')
-	}
+        'includes key1': function(section) {
+          assert.include(section, 'key1')
+          assert.equal(section.key1, 'value1')
+        },
+        'includes key2': function(section) {
+          assert.include(section, 'key2')
+          assert.equal(section.key2, 'value2')
+        }
       }
     }
   },
@@ -67,9 +67,9 @@ vows.describe('c4ini').addBatch({
       topic: parse,
 
       'includes three sections': function(obj) {
-	assert.include(obj, 'section1')
-	assert.include(obj, 'section2')
-	assert.include(obj, 'section3')
+        assert.include(obj, 'section1')
+        assert.include(obj, 'section2')
+        assert.include(obj, 'section3')
       },
 
       section1: includes('section1', ['key1', 'key2']),
@@ -84,39 +84,39 @@ vows.describe('c4ini').addBatch({
       topic: parse,
 
       'includes three top-level sections': function(obj) {
-	assert.include(obj, 'section1')
-	assert.include(obj, 'section2')
-	assert.include(obj, 'section3')
+        assert.include(obj, 'section1')
+        assert.include(obj, 'section2')
+        assert.include(obj, 'section3')
       },
       section1: {
-	topic: function(obj) { return obj.section1 },
+        topic: function(obj) { return obj.section1 },
 
-	'has a key-value pair': function(section) {
-	  assert.equal(section.key, 'value')
-	},
-	'has a subsection': function(section) {
-	  assert.include(section, 'section1_1')
-	},
-	'section1_1': includes('section1_1', ['key'])
+        'has a key-value pair': function(section) {
+          assert.equal(section.key, 'value')
+        },
+        'has a subsection': function(section) {
+          assert.include(section, 'section1_1')
+        },
+        'section1_1': includes('section1_1', ['key'])
       },
       section2: {
-	topic: function(obj) { return obj.section2 },
+        topic: function(obj) { return obj.section2 },
 
-	'has a key-value pair': function(section) {
-	  assert.equal(section.key, 'value')
-	},
-	'has two subsections': function(section) {
-	  assert.include(section, 'section2_1')
-	  assert.include(section, 'section2_2')
-	},
-	'section2_1': {
-	  topic: function(section) { return section['section2_1'] },
+        'has a key-value pair': function(section) {
+          assert.equal(section.key, 'value')
+        },
+        'has two subsections': function(section) {
+          assert.include(section, 'section2_1')
+          assert.include(section, 'section2_2')
+        },
+        'section2_1': {
+          topic: function(section) { return section['section2_1'] },
 
-	  'has another subsection': function(section) {
-	    assert.include(section, 'section2_1_1')
-	  }
-	},
-	'section2_2': includes('section2_2', ['key']),
+          'has another subsection': function(section) {
+            assert.include(section, 'section2_1_1')
+          }
+        },
+        'section2_2': includes('section2_2', ['key']),
       },
       section3: includes('section3', ['key']),
     }
@@ -138,15 +138,15 @@ vows.describe('c4ini').addBatch({
       
       'creates a main section': includes('main', ['key1', 'key2']),
       'creates an array with the remaining sections': function(obj) {
-	assert.includes(obj, 'array')
-	assert.equal(obj.array.length, 3)
+        assert.includes(obj, 'array')
+        assert.equal(obj.array.length, 3)
       },
       'correctly populates the array sections': {
-	topic: function(obj) { return obj.array },
+        topic: function(obj) { return obj.array },
 
-	'0': includes(0, ['key3', 'key4']),
-	'1': includes(1, ['key5', 'key6']),
-	'2': includes(2, ['key7', 'key8']),
+        '0': includes(0, ['key3', 'key4']),
+        '1': includes(1, ['key5', 'key6']),
+        '2': includes(2, ['key7', 'key8']),
       }
     }
   }
